4311afec4b438109734639a8ef451739c297642e
wingo66888 <wingo66888@gmail.com>
2021-03-17 16:45:56 +0800

    新增

--- main.js
+++ main.js
@@ -2,4 +2,9 @@ $("#service").slick({
     autoplay: true,
     autoplaySpeed: 3500,
     slidesToShow: 1
-});
+});$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+      
